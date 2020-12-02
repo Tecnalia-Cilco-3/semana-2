70df3107882a347872957ec6e5dfc44300d0f7c7
Juan Goovaerts <juangoovaerts@gmail.com>
2020-12-01 23:45:24 -0500

    wip

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,14 +1,6 @@
 <template>
-    <div class="card text-white bg-dark">
-        <div class="d-flex justify-content-center p-2">
-            <img :src="member.image">
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Team member: {{member.codigo}} {{member.rol}}</h5>
-            <h5 class="card-title"> {{member.nombre}}</h5>
-            <p class="card-text">{{member.descripcion}}
-            </p>
-        </div>
+    <div>
+
     </div>
 </template>
 
