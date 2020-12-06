d02be82f65f9f70934a825ce9b9a3417a015b1c0
Skyssar <ycure10@gmail.com>
2020-12-05 23:15:50 -0500

    cambios 5-12

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,14 +1,37 @@
 <template>
     <div>
+         
+        <img class="card-img-top" v-bind:src="member.image" alt="">
+        
+        <div class="card-body">
+            <h5 class="card-title text-center mb-0">{{ member.nombre }}</h5>
+            <p class="card-text text-center"><small class="text-muted">{{ member.rol }}</small></p>
 
+            <hr>
+
+            <p class="card-text mb-1"><small class="text-muted"> Código 00{{ member.codigo }}</small></p>
+            <p style="font-size: 13px;">{{ member.descripcion }}</p>
+
+        </div>
+ 
     </div>
 </template>
 
 <script>
-    export default {
-        name: "TeamCard",
-        props: ['member']
-    }
+
+// import datos from "../assets/json/members.json";
+
+export default {
+    name: "TeamCard",
+    props: ['member']
+    // computed: {
+    //     items() {
+    //     return datos.map((item) => {
+    //         return item;
+    //     })
+    //     }
+    // }
+}
 </script>
 
 <style scoped>
