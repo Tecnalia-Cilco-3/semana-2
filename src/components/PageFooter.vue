7f908dc03136660539c10c26c12a249c9f09fe54
= <gclaros@unicauca.edu.co>
2020-12-06 16:01:51 -0500

    organizar footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,30 +1,35 @@
 <template>
-<div>
+
+  <div>
     <!-- Aquí va el footer -->
     <footer>
-    <div class="row">
-      <div class="col-sm-6">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Espacio dedicado a los frameworks para javascript de mayor tendencia y versatilidad en el mundo de la programación.</h5>            
-            <a href="https://github.com/equipo73mintic/retoIcicloIII" class="btn btn-primary" target="_blank">Repositorio Github</a>            
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">
+                Espacio dedicado a los frameworks para javascript de mayor
+                tendencia y versatilidad en el mundo de la programación.
+              </h5>
+              <a
+                href="https://github.com/equipo73mintic/retoIcicloIII"
+                class="btn btn-primary"
+                target="_blank"
+                >Repositorio Github</a
+              >
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    
-</footer>
-
-</div>
+    </footer>
+  </div>
+  
 </template>
 
 <script>
 export default {
   name: "PageHeader",
-  props: {
-
-  },
-  
+  props: {},
 };
 </script>
 
