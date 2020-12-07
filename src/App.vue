1a5f3fc7cd831e562049ac2974917ebe3c8ac14f
JasonR <jerobertoc@udistrital.edu.co>
2020-12-06 23:22:51 -0500

    estructura del codigo, cambio de carpeta imagenes, style section team

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,14 @@
       <hr>
       <h2 class="text-center">Equipo de trabajo</h2>
       <div class="card-deck">
-        <div class="card m-1" v-for="(item, index) of team" v-bind:key="index">
+        <div class="card m-n2" v-for="(item, index) of team" v-bind:key="index">
           <team-card :member="item"></team-card>
         </div>
       </div>
     </section>
 
     <div class="mt-2">
-      <page-footer/>
+      <page-footer></page-footer>
     </div>
 
 
@@ -31,8 +31,8 @@
 <script>
 
 import PageHeader from './components/PageHeader.vue';
-import TeamCard from './components/TeamCard.vue';
 import PageNews from './components/PageNews.vue';
+import TeamCard from './components/TeamCard.vue';
 import PageFooter from './components/PageFooter.vue';
 
 export default {
@@ -52,7 +52,7 @@ export default {
                 nombre : "Wolfgang Santamaría" ,
                 descripcion : "Ingeniero, Músico y sobre apasionado a los videojuegos." ,
                 rol : "Scrum Master" ,
-                image : "images/wolf.jpeg"
+                image : require('../src/assets/images/wolf.jpeg')
                 
             },
             {
@@ -60,28 +60,28 @@ export default {
                 nombre : "Yasar José Cure" ,
                 descripcion : "Estudiante de Ingeniería de Sistemas. Me gusta la comida, jugar fútbol y los videojuegos, pero eso cuando hay tiempo." ,
                 rol : "Desarrollador backend" ,
-                image : "images/yaser.jpeg"
+                image : require('../src/assets/images/yaser.jpeg')
             },
             {
                 codigo : 3 ,
                 nombre : "Geraldine Claros" ,
                 descripcion : "Estudiante de Ingeniería Física. Amante a los animales y la tecnología" ,
                 rol : "Desarrollador frontend" ,
-                image : "images/geral.jpeg"
+                image : require('../src/assets/images//geral.jpeg')
             },
             {
                 codigo : 4,
                 nombre : "Jason Roberto Camargo" ,
                 descripcion : "Apasionado por el desarrollo de software y la tecnologia. Amante a la velocidad. Biker " ,
-                rol : "Documentador" ,
-                image : "images/jason.jpeg"
+                rol : "Desarrolador Fullstack" ,
+                image : require('../src/assets/images/jason.jpeg')
             },
             {
                 codigo : 5 ,
                 nombre : "Jaime Andrés Bravo" ,
                 descripcion : "Ingeniero mecanico, baterista, ciclista, y exfutbolista. Incursionando en el desarrollo de aplicaciones. Con experiencia en el montaje de calderas" ,
                 rol : "Diseñador Gráfico" ,
-                image : "images/andres.jpeg"
+                image : require('../src/assets/images/andres.jpeg')
             }
         ]
       }
--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -6,7 +6,7 @@
       <div class="row">
           <div class="col-sm-12 col-lg-6">            
               <div class="card h-100">
-                <div class="card-header">
+                <div class="card-footer">
                   <h5 class="card-title text-center">Espacio dedicado a los frameworks para javascript de mayor tendencia y versatilidad en el mundo de la programación.</h5>               
                   <p class="text-center"><a href="https://github.com/equipo73mintic/Reto-Semana-2" class="btn btn-info" target="_blank">Repositorio Github</a></p>
                 </div>
@@ -38,7 +38,7 @@
 
 <script>
 export default {
-  name: "PageHeader",
+  name: "PageFooter",
   props: {},
 };
 </script>
