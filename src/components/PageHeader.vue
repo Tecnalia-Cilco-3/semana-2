a03f75b76cc0ee7fa0ab90be4742e85bb404a69f
JasonR <jerobertoc@udistrital.edu.co>
2020-12-07 08:19:23 -0500

    correccion path imagenes header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,7 +4,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         
         <a class="navbar-brand" href="#">
-            <img src="images/js_logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            <img src="../../src/assets/images/js_logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
             Vue JS Page
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
