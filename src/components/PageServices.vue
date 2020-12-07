38379c1d204fcf2a473033d48acde20b0e034070
= <gclaros@unicauca.edu.co>
2020-12-07 14:24:16 -0500

    cambios imagen servicios

--- src/components/PageServices.vue
+++ src/components/PageServices.vue
@@ -1,90 +1,86 @@
 <template>
   <div class="container-fluid">
-
     <div class="row justify-content-center mb-5">
       <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
         <div class="card">
           <div class="d-flex justify-content-center p-2">
-            <svg
-              width="12em"
-              height="12em"
-              viewBox="0 0 16 16"
-              class="bi bi-alarm-fill"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"
-              />
-            </svg>
+            <img
+              src="https://www.itsitio.com/wp-content/uploads/2019/04/soft-covid-780x405.jpg"
+              class="img-fluid"
+              alt="Responsive image"
+            />
           </div>
           <div class="card-body">
-            <h5 class="card-title">Desarrolladores de software fueron muy productivos durante crisis de COVID-19</h5>
+            <h5 class="card-title">
+              Desarrolladores de software fueron muy productivos durante crisis
+              de COVID-19
+            </h5>
             <p class="card-text">
-              Los equipos de desarrolladores de software ser han convertido en el ejemplo
-              de cómo deben operar las empresas después de la crisis del COVID-19.
+              Los equipos de desarrolladores de software ser han convertido en
+              el ejemplo de cómo deben operar las empresas después de la crisis
+              del COVID-19.
             </p>
-            <a href="https://www.enter.co/especiales/dev/desarrolladores-de-software-fueron-muy-productivos-durante-crisis-de-covid-19/" class="btn btn-primary" target="_blank" >More</a>
+            <a
+              href="https://www.enter.co/especiales/dev/desarrolladores-de-software-fueron-muy-productivos-durante-crisis-de-covid-19/"
+              class="btn btn-primary"
+              target="_blank"
+              >More</a
+            >
           </div>
         </div>
       </div>
       <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
         <div class="card">
           <div class="d-flex justify-content-center p-2">
-            <svg
-              width="12em"
-              height="12em"
-              viewBox="0 0 16 16"
-              class="bi bi-alarm-fill"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"
-              />
-            </svg>
+            <img
+              src="https://www.muycomputer.com/wp-content/uploads/2020/12/Mejores-aplicaciones-Google-Play-Store-2020-Android-e1606905702513.jpg"
+              class="img-fluid"
+              alt="Responsive image"
+            />
           </div>
           <div class="card-body">
             <h5 class="card-title">Las mejores apps de Google Play en 2020</h5>
             <p class="card-text">
-              Los editores de Google Play realizaron una lista de las mejores apps 
-              en 2020 y nos pareció importante compartirlas con ustedes.
+              Los editores de Google Play realizaron una lista de las mejores
+              apps en 2020 y nos pareció importante compartirlas con ustedes.
             </p>
-            <a href="https://www.enter.co/chips-bits/apps-software/las-mejores-apps-de-google-play-en-2020/" class="btn btn-primary" target="_blank">More</a>
+            <a
+              href="https://www.enter.co/chips-bits/apps-software/las-mejores-apps-de-google-play-en-2020/"
+              class="btn btn-primary"
+              target="_blank"
+              >More</a
+            >
           </div>
         </div>
       </div>
       <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
         <div class="card">
           <div class="d-flex justify-content-center p-2">
-            <svg
-              width="12em"
-              height="12em"
-              viewBox="0 0 16 16"
-              class="bi bi-alarm-fill"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"
-              />
-            </svg>
+            <img
+              src="https://www.ucaldas.edu.co/portal/wp-content/uploads/2020/08/mision-tic.jpg"
+              class="img-fluid"
+              alt="Responsive image"
+            />
           </div>
           <div class="card-body">
-            <h5 class="card-title">MinTIC busca formar 100.000 programadores para 2025</h5>
+            <h5 class="card-title">
+              MinTIC busca formar 100.000 programadores para 2025
+            </h5>
             <p class="card-text">
-              La iniciativa del Ministerio de TIC enmarcada en el proyecto Jóvenes 4.0, espera 
-              formar entre 68.000 y 112.000 programadores para el 2025.
+              La iniciativa del Ministerio de TIC enmarcada en el proyecto
+              Jóvenes 4.0, espera formar entre 68.000 y 112.000 programadores
+              para el 2025.
             </p>
-            <a href="https://www.enter.co/especiales/dev/mintic-busca-formar-100-000-programadores-para-2025/" class="btn btn-primary" target="_blank">More</a>
+            <a
+              href="https://www.enter.co/especiales/dev/mintic-busca-formar-100-000-programadores-para-2025/"
+              class="btn btn-primary"
+              target="_blank"
+              >More</a
+            >
           </div>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
